f577c37aa832bf7a90f10f7b1de8f2bc4e9c97bd
Prakhar-Ordex <prakhar.tripathi.ordex@gmail.com>
2025-03-04 14:43:01 +0530

    redux toolkit setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App';
+import { store } from "./store/store"
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
